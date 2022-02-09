b31de490117c723c16c340460c42e0c1cac27542
Clément Chevallier <clementinise@gmail.com>
2022-02-09 17:03:36 +0100

    Initial release

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,11 +1,14 @@
 fx_version 'cerulean'
 game 'gta5'
 
+author 'Clementinise'
+name '^2FiveM Vehicle Loader'
+description 'Load the addon vehicles while your player are joining the server (To prevent invisible vehicles)'
+github 'https://github.com/clementinise/fivem-vehicleloader'
+version '1.0'
+
 shared_script 'config.lua'
 client_script 'client.lua'
 server_script 'server.lua'
 
 fivem_checker 'yes'
-github 'https://github.com/clementinise/fivem-vehicleloader'
-version '1.0'
-name '^2FiveM Vehicle Loader'
